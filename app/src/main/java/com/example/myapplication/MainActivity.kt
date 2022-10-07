d4e06f04faecfe6b6a1b9d66b0b8d69ee1fcb361
Han-JeongHyeon <fldkej131@gmail.com>
2022-10-07 16:52:38 +0900

    api9 tack

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -54,78 +54,89 @@ class MainActivity : AppCompatActivity() {
 
         nameArray = SparseArray<DateClassTest>()
 
-//        var query = "DELETE FROM animals;"
+//        var query = "delete from animals;"
 //        database.execSQL(query)
 
         asyncTask.execute()
 
     }
 
-    val asyncTask = object : AsyncTask<Void, Int, String>() {
-        @SuppressLint("Range")
-        override fun doInBackground(vararg params: Void?): String {
-            val retrofit = Retrofit.Builder().baseUrl("https://acnhapi.com/v1/")
-                .addConverterFactory(GsonConverterFactory.create()).build();
-            val service = retrofit.create(FishName_interface::class.java);
+    fun apiCallBack(callback : (SparseArray<DateClassTest>) -> Unit){
+        val retrofit = Retrofit.Builder().baseUrl("https://acnhapi.com/v1/")
+            .addConverterFactory(GsonConverterFactory.create()).build();
+        val service = retrofit.create(FishName_interface::class.java);
+        for (i in 0..80) {
+            service.getName("${i + 1}").enqueue(object: Callback<FishName>{
+                //api 요청 실패 처리
+                override fun onFailure(call: Call<FishName>, t: Throwable) {
+                    Log.d("Error", ""+t.toString())
+                }
+                //api 요청 성공 처리
+                override fun onResponse(call: Call<FishName>, response: Response<FishName>) {
+                    var result: FishName? = response.body()
+                    //Array에 값 저장
+//                    if(i % 2 == 0){
+//                        var query = "INSERT INTO animals('num','name','price','image') values('${i}','${null}','${null}','${null}');"
+//                        database.execSQL(query)
+//                        nameArray?.append(i,DateClassTest("${result?.name?.KRko}","${result?.price}","${result?.image}"))
+//                    }
+//                    else{
+                        var query = "INSERT INTO animals('num','name','price','image') values('${i}','${result?.name?.KRko}','${result?.price}','${result?.image}');"
+                        database.execSQL(query)
+                        nameArray?.append(i,DateClassTest("${result?.name?.KRko}","${result?.price}","${result?.image}"))
+//                    }
+                    Log.d("TAG", "$i")
+                    if (nameArray?.size() == 80) {
+                        callback(nameArray!!)
+                    }
+                }
+            })
+        }
+        Log.d("TAG", "100")
+    }
 
-            CoroutineScope(Dispatchers.IO).launch {
-                var query = "SELECT * FROM animals ORDER BY num asc;"
-                var cursor = database.rawQuery(query, null)
-                if(cursor.count == 0){
-                    for (i in 0 until 80) {
-                        service.getName("${i + 1}").enqueue(object: Callback<FishName>{
-                            //api 요청 실패 처리
-                            override fun onFailure(call: Call<FishName>, t: Throwable) {
-                                Log.d("Error", ""+t.toString())
-                            }
-                            //api 요청 성공 처리
-                            override fun onResponse(call: Call<FishName>, response: Response<FishName>) {
-                                var result: FishName? = response.body()
-                                //Array에 값 저장
-                                var query = "INSERT INTO animals('num','name','price','image') values('${i}','${result?.name?.KRko}','${result?.price}','${result?.image}');"
-                                database.execSQL(query)
-                                nameArray?.append(i,DateClassTest("${result?.name?.KRko}","${result?.price}","${result?.image}"))
-                                if (nameArray?.size() == 80) {
-                                    addDataToRecyclerView()
-                                }
-                            }
-                        })
-                    }
-                }
-                else{
-                    while(cursor.moveToNext()){
-                        var num = cursor.getString(cursor.getColumnIndex("num"))
-                        var name = cursor.getString(cursor.getColumnIndex("name"))
-                        var price = cursor.getString(cursor.getColumnIndex("price"))
-                        var image = cursor.getString(cursor.getColumnIndex("image"))
-                        nameArray?.append(num.toInt(),DateClassTest(name,price,image))
-                        if (nameArray?.size() == 80) {
-                            addDataToRecyclerView()
-                        }
-                    }
-                }
+    val asyncTask = object : AsyncTask<Void, Int, SparseArray<DateClassTest>>() {
+
+        @SuppressLint("Range")
+        override fun doInBackground(vararg params: Void?): SparseArray<DateClassTest> {
+            var nameArray : SparseArray<DateClassTest> = SparseArray()
+
+            var query = "SELECT * FROM animals ORDER BY num asc;"
+            var cursor = database.rawQuery(query, null)
+            while(cursor.moveToNext()){
+                var num = cursor.getString(cursor.getColumnIndex("num"))
+                var name = cursor.getString(cursor.getColumnIndex("name"))
+                var price = cursor.getString(cursor.getColumnIndex("price"))
+                var image = cursor.getString(cursor.getColumnIndex("image"))
+                nameArray?.append(num.toInt(),DateClassTest(name,price,image))
             }
 
-            return "완료"
+            return nameArray
         }
 
         override fun onProgressUpdate(vararg values: Int?) {
             super.onProgressUpdate(*values)
-
-            binding.text.text = "${values[0]!!}"
         }
 
-        override fun onPostExecute(result: String?) {
+        override fun onPostExecute(result: SparseArray<DateClassTest>) {
             super.onPostExecute(result)
-            nameArray?.let {
-                fishAdapter.datas = it
-                fishAdapter.notifyDataSetChanged()
-            }
+            setRecyclerView(result)
         }
     }
 
-    private fun addDataToRecyclerView() {
-        nameArray?.let {
+    fun setRecyclerView(result : SparseArray<DateClassTest>) {
+        if (result.size() == 0){
+           apiCallBack {
+               addDataToRecyclerView(it)
+           }
+        }
+        else{
+            addDataToRecyclerView(result)
+        }
+    }
+
+    private fun addDataToRecyclerView(result : SparseArray<DateClassTest>) {
+        result?.let {
             fishAdapter.datas = it
         }
         fishAdapter.notifyDataSetChanged()
