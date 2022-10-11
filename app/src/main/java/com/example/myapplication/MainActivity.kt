392c118f75e8b246b4c16e4fff46636ab4d88e0a
Han-JeongHyeon <fldkej131@gmail.com>
2022-10-11 12:22:45 +0900

    api12 tack

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,7 +5,6 @@ import android.database.sqlite.SQLiteDatabase
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.util.Log
 import android.util.SparseArray
 import android.widget.Toast
@@ -24,24 +23,13 @@ import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
-    //뷰 바인딩
     private lateinit var binding: ActivityMainBinding
-    // SQLite
-    // Room db
-    // CamelCase
-    //어뎁터 연결
+
     lateinit var fishAdapter: Adapter
     val datas = mutableListOf<DateClassTest>()
-//    val datas = SparseArray<DateClassTest>()
-    // SnakeStyle
-    //SparseArray
-//    var nameArray : SparseArray<DateClassTest>? = null
 
     lateinit var dbHelper: SqliteHelper
     lateinit var database: SQLiteDatabase
-
-    var numCheckList =  ArrayList<String>()
-    var numList =  ArrayList<String>()
 
     @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,78 +44,37 @@ class MainActivity : AppCompatActivity() {
         dbHelper = SqliteHelper(this, "FishName.db", null, 1)
         database = dbHelper.writableDatabase
 
-
-//        var query = "delete from animals;"
-//        database.execSQL(query)
-
-//        for (i in 0 until 80) {
-//            arr?.append(i, "${i + 1}")
-//            Log.d("TAG", "asdasdasdsdaaa")
-//        }
-
         asyncTask.execute()
 
     }
 
-    fun apiCallBack(callback : (MutableList<DateClassTest>) -> Unit){
-        val retrofit = Retrofit.Builder().baseUrl("https://acnhapi.com/v1/")
-            .addConverterFactory(GsonConverterFactory.create()).build();
-        val service = retrofit.create(FishName_interface::class.java);
-
+    fun api(numPosition : Int) {
         var j = 0
 
-//        reqcnt=0
-        if (datas.size == 0) {
-            for (i in 1..80) {
-                service.getName("${i}").enqueue(object: Callback<FishName>{
-                    //api 요청 실패 처리
-                    override fun onFailure(call: Call<FishName>, t: Throwable) {
-                        Log.d("Error", ""+t.toString())
-                    }
-                    //api 요청 성공 처리
-                    override fun onResponse(call: Call<FishName>, response: Response<FishName>) {
-                        var result: FishName? = response.body()
-                        //Array에 값 저장
-                        if(i % 2 == 0){
-                            j++
-                        }
-                        else{
-                            var query = "INSERT INTO animals('num','name','price','image') values('${i}','${result?.name?.KRko}','${result?.price}','${result?.image}');"
-                            database.execSQL(query)
-                            datas.apply {
-                                add(DateClassTest("${result?.name?.KRko}","${result?.price}","${result?.image}"))
-                            }
-                        }
-                        if (datas.size+j == 80) {
-                            callback(datas!!)
-                        }
-                    }
-                })
+        val retrofit = Retrofit.Builder().baseUrl("https://acnhapi.com/v1/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        val service = retrofit.create(FishName_interface::class.java)
+
+        service.getName("$numPosition").enqueue(object: Callback<FishName>{
+            //api 요청 실패 처리
+            override fun onFailure(call: Call<FishName>, t: Throwable) {
+                Log.d("Error", ""+t.toString())
             }
-        }
-        else{
-            Log.d("TAG", "2132132 ${numCheckList.size}")
-            for (i in 0 until numCheckList.size) {
-                service.getName("${numCheckList.get(i)}").enqueue(object: Callback<FishName>{
-                    //api 요청 실패 처리
-                    override fun onFailure(call: Call<FishName>, t: Throwable) {
-                        Log.d("Error", ""+t.toString())
+            //api 요청 성공 처리
+            override fun onResponse(call: Call<FishName>, response: Response<FishName>) {
+                var result: FishName? = response.body()
+                if(java.util.Random().nextInt(10) + 1 == 1){
+                    j++
+                }
+                else{
+                    var query = "INSERT INTO animals('num','name','price','image') values('${numPosition}','${result?.name?.KRko}','${result?.price}','${result?.image}')"
+                    database.execSQL(query)
+                    datas.apply {
+                        add(DateClassTest("${result?.name?.KRko}","${result?.price}","${result?.image}"))
                     }
-                    //api 요청 성공 처리
-                    override fun onResponse(call: Call<FishName>, response: Response<FishName>) {
-                        var result: FishName? = response.body()
-                            var query = "INSERT INTO animals('num','name','price','image') values('${numCheckList.get(i)}','${result?.name?.KRko}','${result?.price}','${result?.image}');"
-                            database.execSQL(query)
-                            datas.apply {
-                                add(DateClassTest("${result?.name?.KRko}","${result?.price}","${result?.image}"))
-                            }
-                        if (datas.size == 80) {
-                            callback(datas!!)
-                        }
-                    }
-                })
+                }
             }
-        }
+        })
 
     }
 
@@ -137,7 +84,6 @@ class MainActivity : AppCompatActivity() {
 
         @SuppressLint("Range")
         override fun doInBackground(vararg params: Void?): MutableList<DateClassTest> {
-            Log.d("TAG", "doInBackground1")
             var query = "SELECT * FROM animals order by num;"
             var cursor = database.rawQuery(query, null)
             while(cursor.moveToNext()){
@@ -145,21 +91,27 @@ class MainActivity : AppCompatActivity() {
                 var name = cursor.getString(cursor.getColumnIndex("name"))
                 var price = cursor.getString(cursor.getColumnIndex("price"))
                 var image = cursor.getString(cursor.getColumnIndex("image"))
-                datas.add(DateClassTest("${name}","${price}","${image}"))
 
-//                numCheckList.add("$num")
-
-                forEnd@ for (i in numCheck..80) {
+                forBreak@ for (i in numCheck..80) {
                     if(num == i){
                         numCheck = num + 1
-                        break@forEnd
+                        datas.add(DateClassTest("$name","$price","$image"))
+                        break@forBreak
                     }
                     else{
-                        numCheckList.add("$i")
+                        api(i)
                     }
                     if(datas.size == 40 && i != 80){
-                        numCheckList.add("80")
+                        api(80)
                     }
+                }
+            }
+
+            if (datas.size == 0) {
+                var i = 1
+                while (i <= 80){
+                    api(i)
+                    i++
                 }
             }
 
@@ -177,20 +129,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setRecyclerView(result : MutableList<DateClassTest>) {
-        if (result.size == 0){
-           apiCallBack {
-               addDataToRecyclerView(it)
-           }
-        }
-        else if (result.size <= 79){
-            addDataToRecyclerView(result)
-            apiCallBack {
-                addDataToRecyclerView(it)
-            }
-        }
-        else{
-            addDataToRecyclerView(result)
-        }
+        addDataToRecyclerView(result)
     }
 
     private fun addDataToRecyclerView(result : MutableList<DateClassTest>) {
